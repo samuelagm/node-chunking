76d57c8a6c96b600d4af7bbeedbc60b25c848c62
Thuong Dinh <thuongdinh99@gmail.com>
2013-01-10 08:33:59 +0400

    finish core: convert grammar to XRegExp

--- test/core-test.js
+++ test/core-test.js
@@ -20,15 +20,35 @@ vows.describe('node-chunking/core').addBatch({
         	// should not come to this line
         	assert.isFalse(true);
         },
-        'and when do call init with default grammar': function (err, chunking) {
-        	chunking.init();
+        'and when do call private method _devideGrammarLevel & _preprocessGrammarSubPatterns': function (err, chunking) {
+            //_preprocessGrammarSubPatterns
+            var preprocessGrammarSubPatterns = chunking._preprocessGrammarSubPatterns(chunking.grammar); 
+            assert.isNotNull(preprocessGrammarSubPatterns);
+            assert.equal(preprocessGrammarSubPatterns.processedGrammar['NP'], '({{DT}}|{{JJ}}|{{NN}}.*)+');
+            assert.equal(preprocessGrammarSubPatterns.processedGrammar['PP'], '({{IN}})({{NP}})');
+            assert.equal(preprocessGrammarSubPatterns.processedGrammar['VP'], '({{VB}}.*)({{NP}}|{{PP}})+$');
+            assert.equal(preprocessGrammarSubPatterns.grammarKeys, '{{NP}}|{{PP}}|{{VP}}');
 
-            var processedGrammar = chunking.processedGrammar;
+            //_devideGrammarLevel
+            var grammarKeyLevels = chunking._devideGrammarLevel(preprocessGrammarSubPatterns);
+            assert.isNotNull(grammarKeyLevels);
+            assert.deepEqual(grammarKeyLevels.level1, ['NP']);
+            assert.deepEqual(grammarKeyLevels.level2, ['PP']);
+            assert.deepEqual(grammarKeyLevels.level3, ['VP']);
+        },
+        'and when do call private method _preprocessGrammar': function (err, chunking) {
+            var processedGrammar = chunking._preprocessGrammar(chunking.grammar);
 
         	assert.isNotNull(processedGrammar);
-            assert.equal(processedGrammar['NP'], '({{DT}}|{{JJ}}|{{NN}}.*)+');
-            assert.equal(processedGrammar['PP'], '({{IN}})({{NP}})');
-            assert.equal(processedGrammar['VP'], '({{VB}}.*)({{NP}}|{{PP}})+$');
+            assert.equal('({{DT}}|{{JJ}}|{{NN}}.*)+', processedGrammar['NP']);
+            assert.equal('({{IN}})(({{DT}}|{{JJ}}|{{NN}}.*)+)', processedGrammar['PP']);
+            assert.equal('({{VB}}.*)(({{DT}}|{{JJ}}|{{NN}}.*)+|({{IN}})(({{DT}}|{{JJ}}|{{NN}}.*)+))+$', processedGrammar['VP']);
+        },
+        'and when do call init': function (err, chunking) {
+            chunking.init();
+            var processedGrammar = chunking.processedGrammar;
+
+            assert.isNotNull(processedGrammar);
         },
     }
 }).export(module);
